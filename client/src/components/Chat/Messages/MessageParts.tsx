c06d4fa8d43a3235946b71deea44fbdce8bbb34d
Yuval Shirav <yuvalshirav@gmail.com>
2024-04-24 01:30:41 -0400

    hide user icons

--- client/src/components/Chat/Messages/MessageParts.tsx
+++ client/src/components/Chat/Messages/MessageParts.tsx
@@ -44,7 +44,7 @@ export default function Message(props: TMessageProps) {
       >
         <div className="m-auto justify-center p-4 py-2 text-base md:gap-6 ">
           <div className="group mx-auto flex flex-1 gap-3 text-base md:max-w-3xl md:px-5 lg:max-w-[40rem] lg:px-1 xl:max-w-[48rem] xl:px-5">
-            <div className="relative flex flex-shrink-0 flex-col items-end">
+            <div className="relative flex flex-shrink-0 flex-col items-end hidden">
               <div>
                 <div className="pt-0.5">
                   <div className="shadow-stroke flex h-6 w-6 items-center justify-center overflow-hidden rounded-full">
